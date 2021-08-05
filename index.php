e522e427ac32132b0941232ac60c428821b3d5f8
Melodious-nub <suffixit003@gmail.com>
2021-08-06 04:42:00 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>PHP</title>
+    </head>
+</html>
+<body>
+    <!-- Basic HTML -->
+    <h1>This IS a PHP Site</h1>
+    <br>
+    <?php
+    // printing to html using echo 
+    echo 'Hello Php';
+    // can use html code in echo
+    echo '<br>';
+    echo 'Hello Kasem';
+    echo '<br>';
+    ?>
+
+    <?php
+    // php variable in $ signe
+    $name = 'Shawon Talukder';
+    $age = 24;
+    
+    ?>
+
+</body>
