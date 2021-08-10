99c4f346fd650e51d77b93f001ba5b06e58e12c2
Melodious-nub <suffixit003@gmail.com>
2021-08-10 12:53:34 +0600

    [Added File]

---
+++ arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Array</title>
+</head>
+<body>
+    <h1>Arrys</h1>
+    <?php
+    // This is a Variable
+    $num = 100;
+    // This is a Array
+    $numbers = array(1,3,5,7,9,11,13,15,17,19,20);
+    // Can Use Acess the value in a Subscript of the array Using Index
+    echo $numbers[0];
+
+    echo "<p>$numbers[5]</p>";
+
+    ?>
+    
+</body>
+</html>
